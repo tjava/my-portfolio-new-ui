3f88fb0c6c717f006de2cc31be591479ff923a74
tjava <mustyytee@gmail.com>
2022-02-23 15:26:31 +0100

    home page completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+import 'package:my_portfolio_ui/constants/colors.dart';
 import 'package:my_portfolio_ui/widgets/custom_text.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,8 +8,143 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CustomText(text: "Home Page"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              width: 220,
+              margin: const EdgeInsets.only(top: 80),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: const DecorationImage(
+                  image: AssetImage("assets/images/me.png"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            CustomText(
+              text: "Taiwo Hassan",
+              size: 25,
+              weight: FontWeight.bold,
+              color: dark,
+            ),
+            SizedBox(height: 10),
+            CustomText(
+              text: "Software Engineer",
+              size: 15,
+              weight: FontWeight.w500,
+              color: lightGrey,
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image:
+                                AssetImage("assets/images/homepagePurple.png"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text:
+                              "Hi I'm Taiwo. I'm a full stack developer who loves to play around with programming and learning new things.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: dark,
+                          ),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 80),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  height: 70,
+                                  width: 70,
+                                  child: Image(
+                                    image:
+                                        AssetImage("assets/images/wrench.png"),
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                CustomText(
+                                  text: "Projects",
+                                  size: 18,
+                                  weight: FontWeight.bold,
+                                  color: Colors.purple,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  height: 70,
+                                  width: 70,
+                                  child: Image(
+                                    image:
+                                        AssetImage("assets/images/github.png"),
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                CustomText(
+                                  text: "Github",
+                                  size: 18,
+                                  weight: FontWeight.bold,
+                                  color: Colors.purple,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  height: 70,
+                                  width: 70,
+                                  child: Image(
+                                    image: AssetImage("assets/images/cogs.png"),
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                CustomText(
+                                  text: "Skills",
+                                  size: 18,
+                                  weight: FontWeight.bold,
+                                  color: Colors.purple,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
